1269b31ba8dd76470053be7d003ddc5b90f5dc54
moebius surfing <moebiussurfing@gmail.com>
2021-08-19 05:08:56 -0300

    +

--- src/NoiseAnimator.cpp
+++ src/NoiseAnimator.cpp
@@ -543,8 +543,8 @@ void NoiseAnimator::setup()
 	//--
 
 	// plot
-	plotShape = ImVec2(210, 200);
-	//plotShape = ImVec2(210, 315);
+	//plotShape = ImVec2(210, 200);
+	plotShape = ImVec2(210, 315);
 	ofFbo::Settings fboSettings;
 	fboSettings.width = plotShape.x;
 	fboSettings.height = plotShape.y;
